ffd163c1fdc0dbb6a7de33e2560c7e3c312f1e0e
andlas98 <andrewohakam@outlook.com>
2024-11-03 19:41:04 -0500

    More responsive Technical Writing Card

--- src/components/technicalWritingCard.jsx
+++ src/components/technicalWritingCard.jsx
@@ -3,8 +3,8 @@ import { Button, Box, Typography} from "@mui/material"
 export default function TechnicalWritingCard(props){
     let { documentTitle, documentDescription, documentLink } = props;
     return(
-        <Box className="w-[60vw] bg-maroon flex rounded-reg p-[1.5rem] mb-[2rem] m-auto">
-            <Box className="w-[80%]">
+        <Box className="w-[60vw] bg-maroon flex rounded-reg p-[1.5rem] mb-[2rem] m-auto max-md:flex-col">
+            <Box className="w-[80%] max-md:w-full">
                 <Typography className="technicalWritingCardTitle font-bold mb-[1.5rem]">
                     {documentTitle}
                 </Typography>
@@ -12,8 +12,8 @@ export default function TechnicalWritingCard(props){
                 {documentDescription}
                 </Typography>
             </Box>
-            <Box className="w-auto flex align-top mx-auto">
-                <Button className="bg-deepRed h-fit align-center hover:bg-deepRed" href={documentLink}>
+            <Box className="w-auto flex align-top mx-auto max-md:mt-[1rem]">
+                <Button className="bg-deepRed h-fit align-center hover:bg-deepRed max-md:px-[2rem]" href={documentLink}>
                     <Typography className="technicalWritingCardButton text-white" variant="button">
                         View
                     </Typography>

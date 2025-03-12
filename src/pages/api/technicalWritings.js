18b60e3f41d099b9f1c26cf25a55c505321dd632
andlas98 <andrewohakam@outlook.com>
2025-03-12 12:10:53 -0400

    Adding netlify.toml

--- src/pages/api/technicalWritings.js
+++ src/pages/api/technicalWritings.js
@@ -1,18 +1,26 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI; // Ensure you have your MongoDB URI in your .env.local file
-const client = new MongoClient(uri);
+let client;
+let clientPromise;
+
+if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    global._mongoClientPromise = client.connect();
+}
+clientPromise = global._mongoClientPromise;
 
 export default async function handler(req, res) {
     try {
-        await client.connect();
+        const client = await clientPromise;
         const database = client.db("database1");
         const collection = database.collection("technical_writing_collection");
         const technicalWritings = await collection.find({}).toArray();
         res.status(200).json(technicalWritings);
     } catch (error) {
         res.status(500).json({ error: "Failed to fetch data" });
-    } finally {
-        await client.close();
     }
 }

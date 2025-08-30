05f9d69d43c05d72200ac2e6a5e699531a6c7e3b
andlas98 <andrewohakam@outlook.com>
2025-08-29 22:45:55 -0400

    Removed some MongoDB references, slight recolor

--- src/components/layeredPaper.jsx
+++ src/components/layeredPaper.jsx
@@ -5,10 +5,10 @@ import { Paper } from "@mui/material";
 function LayeredPaper(props){
     return (
         <div className="layeredPaperDiv">
-            <Paper className="bgPaper w-full rounded-[0.625rem] text-[1.8rem] py-[3rem] px-[2.813rem] shadow-none bg-[#0E0E0E] text-transparent;">
+            <Paper className="bgPaper w-full rounded-[0.625rem] text-[1.8rem] py-[3rem] px-[2.813rem] shadow-none bg-[#1A1A1A] text-transparent;">
                 {props.content}
             </Paper>
-            <Paper className="fgPaper w-full rounded-[0.625rem] text-[1.8rem] py-[3rem] px-[2.813rem] shadow-none bg-[#141414] text-white">
+            <Paper className="fgPaper w-full rounded-[0.625rem] text-[1.8rem] py-[3rem] px-[2.813rem] shadow-none bg-[#1F1F1F] text-white">
                 {props.content}
             </Paper>
         </div>
